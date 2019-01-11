62d395ebb2dfaa960b3d76466a58084e6180c430
erichanliu <ericliu404@gmail.com>
2019-01-11 00:12:23 -0500

    New: Add completed search with favorite stars
    Todo: implement the favorite display properly

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var data;
 
+// fetch the JSON file and store the results in the data variable
 function fetchJSON (url)
 {
     fetch(url, {
@@ -19,6 +20,7 @@ function fetchJSON (url)
     });    
 }
 
+// lookup the query through the set of keywords in the data object
 function lookup (query) 
 {
     let results = [];
@@ -33,15 +35,121 @@ function lookup (query)
     return results;
 }
 
-$("#doSearch").click(function ()
+
+
+// lookup the search box value  
+function doQuery ()
 {
-    let query = document.getElementById("search").value;
+    let query = document.getElementById("searchBar").value;
 
     let results = lookup(query);
 
-    document.getElementById("output").innerText = results;
+    printResults(results);
+    // document.getElementById("output").innerText = results;
+}
+
+$("#searchButton").click(doQuery);
+
+// listen for enter keypress
+$("#searchBar").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+        doQuery();
+    }
 });
 
-const TORONTOWASTEURL = "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000";
-fetchJSON(TORONTOWASTEURL);
+// decodes the html entities in the return data to be actual html tags
+function decodeHTML (html)
+{
+    let txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
 
+function printResults (results)
+{ 
+    // print each result in a div
+    for (let i = 0, len = results.length; i < len; i++) 
+    {
+        let res = results[i];
+
+        let container = document.createElement("div");
+        let leftDiv = document.createElement("div");
+        let rightDiv = document.createElement("div");
+        let leftText = document.createElement("p");
+        let rightText = document.createElement("p");
+        let starIcon = document.createElement("i");
+
+        container.classList.add("row");
+        container.classList.add("my-1");
+        leftDiv.classList.add("col-5");
+        rightDiv.classList.add("col-7");
+
+        starIcon.classList.add("far");
+        starIcon.classList.add("fa-star");
+        starIcon.onclick = function () { favorite (starIcon, res); };
+
+        // render star based on if element is in favorite list
+        leftText.innerHTML = decodeHTML(res.title); 
+        rightText.innerHTML = decodeHTML(res.body);
+
+        leftDiv.appendChild(leftText);
+        leftDiv.appendChild(starIcon);
+        rightDiv.appendChild(rightText);
+        container.appendChild(leftDiv);
+        container.appendChild(rightDiv);
+
+        document.getElementById("output").appendChild(container);
+    }
+}
+
+// favorite the element
+let favorites = [];
+function favorite (icon, res)
+{
+    // add to favorites list, fill color
+    favorites.push(res);
+    icon.classList.remove("far");
+    icon.classList.add("fas");
+
+    icon.onclick = function () { unfavorite(icon, res); };
+
+    updateFavoritesDisplay();
+}
+
+function unfavorite (icon, res)
+{
+    // remove from favorites array, remove fill
+    favorites.splice(favorites.indexOf(res), 1);
+    icon.classList.remove("fas");
+    icon.classList.add("far");
+
+    icon.onclick = function () { favorite(icon, res); };
+
+    updateFavoritesDisplay();
+}
+
+function updateFavoritesDisplay ()
+{
+
+}
+
+// listen for cleared searchbar
+document.getElementById("searchBar").addEventListener("input", function () 
+{
+    if (document.getElementById("searchBar").value === "")
+    {
+        document.getElementById("output").innerText = "";
+    }
+});
+
+
+
+const TORONTOWASTEURL = "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000";
+
+// main function called on page load
+function main ()
+{
+    fetchJSON(TORONTOWASTEURL);
+}
+
+main();
